38e43910286bc294d665c279d682f2a68490720b
Carolyn Smith <casmith205@gmail.com>
2018-04-06 14:02:03 -0500

    added js file with connection to db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,23 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    start();
+});
+
+function start(){
+    
+}
