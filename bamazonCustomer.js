b85248f27195313a871d8306c2ec148bf3fc0a7e
Carolyn Smith <casmith205@gmail.com>
2018-04-06 14:28:54 -0500

    added a buy item function to query user

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("console.table");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -18,6 +19,41 @@ connection.connect(function (err) {
     start();
 });
 
-function start(){
-    
-}
+function start() {
+    connection.query("SELECT * FROM products",
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            buyItem();
+        });
+};
+
+function buyItem() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: "itemList",
+                type: "list",
+                choices: function () {
+                    var itemArr = [];
+                    for (i = 0; i < res.length; i++) {
+                        itemArr.push(res[i].product_name);
+                    };
+                    return (itemArr);
+                },
+                message: "What item would you like to buy?"
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many would you like to buy?"
+            }
+        ]).then(function (userResponse) {
+            console.log(userResponse.itemList);
+            console.log(userResponse.quantity);
+
+        });
+    }
+    )
+};
